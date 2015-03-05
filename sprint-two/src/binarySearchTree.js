92424bb0acccc09dcc9763999dc5d4928a3e7b2c
bryclee <pygmize.me@gmail.com>
2015-03-05 14:11:32 -0800

    Finished BinarySearchTree.js

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,27 +1,54 @@
 var BinarySearchTree = function(value){
-	this.value = value;
-	this.left = null;
-	this.right = null;
-};
+	var tree = Object.create(BinarySearchTree.prototype);
 
-BinarySearchTree.prototype.left = function(){
-  return this.left;
-};
+	tree.value = value;
+	tree.left = null;
+	tree.right = null;
 
-BinarySearchTree.prototype.right = function(){
-  return this.right;
+	return tree;
 };
 
 BinarySearchTree.prototype.insert = function(val){
-
+	// check lower or greater than value
+  if (val < this.value){
+  	// check if branch does not exist and add or
+    if (this.left === null){
+    	this.left = new BinarySearchTree(val);
+    // call insert of that branch
+    } else {
+    	this.left.insert(val);
+    }
+  } else if (val > this.value){
+  	if (this.right === null){
+  		this.right = new BinarySearchTree(val);
+  	} else {
+  		this.right.insert(val);
+  	}
+  }
 };
 
 BinarySearchTree.prototype.contains = function(val){
-
+  if (this.value == val){
+  	return true;
+  } else if (val < this.value){
+    if (this.left === null){
+    	return false;
+    } else {
+    	return this.left.contains(val);
+    }
+  } else if (val > this.value){
+    if (this.right === null){
+    	return false;
+    } else {
+    	return this.right.contains(val);
+    }
+  }
 };
 
 BinarySearchTree.prototype.depthFirstLog = function(cb){
-
+  cb(this.value);
+  this.left && this.left.depthFirstLog(cb);
+  this.right && this.right.depthFirstLog(cb);
 };
 
 /*
