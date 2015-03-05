60ebac694c742db956c2fd22c5e5da7e516102b8
bryclee <pygmize.me@gmail.com>
2015-03-04 16:14:01 -0800

    Added linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,35 @@
 var LinkedList = function(){
+  // create a linked list, using the functional method
   var list = {};
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value){
+    // create a new Node
+    var newNode = Node(value);
+
+    list.tail && (list.tail.next = newNode);
+    list.head || (list.head = newNode);
+    list.tail = newNode;
   };
 
   list.removeHead = function(){
+    // set the head.next node to list.head
+    // delete the head.next node
+    var result = list.head.value;
+    list.head = list.head.next;
+
+    return result;
   };
 
   list.contains = function(target){
+    var idx = list.head;
+    while (idx) {
+      if (idx.value === target)
+        return true;
+      idx = idx.next;
+    }
+    return false;
   };
 
   return list;
