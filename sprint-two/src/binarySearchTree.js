937598174393d9e9645ef36972829c6445c4069f
bryclee <pygmize.me@gmail.com>
2015-03-05 19:50:10 -0800

    Starting the automatic tree balancing problem

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -4,28 +4,72 @@ var BinarySearchTree = function(value){
 	tree.value = value;
 	tree.left = null;
 	tree.right = null;
+  tree.parent = null;
+  tree.depth = 0;
+  tree.width = 0;
 
 	return tree;
 };
 
 BinarySearchTree.prototype.insert = function(val){
 	// check lower or greater than value
+  var depth = 0;
+
   if (val < this.value){
   	// check if branch does not exist and add or
     if (this.left === null){
-    	this.left = new BinarySearchTree(val);
     // call insert of that branch
+    // set parent property of new branch
+    // increase width of this by 1
+    // start depth counter at 1
+      this.left = new BinarySearchTree(val);
+      this.left.parent = this;
+      this.width++;
     } else {
-    	this.left.insert(val);
+    	depth = this.left.insert(val);
     }
   } else if (val > this.value){
   	if (this.right === null){
   		this.right = new BinarySearchTree(val);
+      this.right.parent = this;
+      this.width++;
   	} else {
-  		this.right.insert(val);
+  		depth = this.right.insert(val);
   	}
   }
+  depth++;
+  // increase depth property
+  if (depth > this.depth)
+    this.depth = depth;
+  // check if node is unbalanced goes here
+  return depth;
 };
+
+BinarySearchTree.print = function(){
+  
+}
+/* examples: == depth: 1
+1-
+- -
+=============== depth: 2
+   -
+ -   -
+- - - -
+=============== depth: 3
+       -
+   -       -
+ -   -   -   -
+- - - - - - - -
+=============== depth: 4
+
+
+
+1-123-123-123-123-1
+-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-
+
+spaces = 2d * 1
+*/
+
 
 BinarySearchTree.prototype.contains = function(val){
   if (this.value == val){
@@ -53,13 +97,21 @@ BinarySearchTree.prototype.depthFirstLog = function(cb){
 
 BinarySearchTree.prototype.breadthFirstLog = function(cb){
   var queue = [];
+  var nextQueue = [];
   queue.push(this);
 
-  while (queue.length > 0){
-    var testing = queue.shift();
-    cb(testing.value);
-    (testing.left !== null) && (queue.push(testing.left));
-    (testing.right !== null) && (queue.push(testing.right));
+  // process all in queue first (this level) before starting nextQueue
+  while (queue.length > 0 || nextQueue.length > 0){
+    while (queue.length > 0){
+      var testing = queue.shift();
+      cb(testing.value);
+      (testing.left !== null) && (nextQueue.push(testing.left));
+      (testing.right !== null) && (nextQueue.push(testing.right));
+    }
+    if (nextQueue.length > 0){
+      queue = nextQueue;
+      nextQueue = [];
+    }
   }
 }
 
